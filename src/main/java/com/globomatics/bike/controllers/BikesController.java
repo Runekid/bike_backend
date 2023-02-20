87f3e236f33584eaa16bea68a0c0db18b54e09d4
Onur Bugdayci <bugdayci.onur@aol.com>
2023-02-20 15:52:10 +0100

    added persistence layer correctly this time woop woop

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -1,27 +1,33 @@
 package com.globomatics.bike.controllers;
 
 import com.globomatics.bike.models.Bike;
+import com.globomatics.bike.repositories.BikeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/bikes")
+@RequiredArgsConstructor
 public class BikesController {
+
+    private final BikeRepository bikeRepository;
+
     @GetMapping
     public List<Bike> list() {
-        return new ArrayList<>();
+        return bikeRepository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Bike bike) {
+        bikeRepository.save(bike);
     }
 
     @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id) {
-        return new Bike();
+        return bikeRepository.findById(id).orElse(null);
     }
 }
--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -1,18 +1,33 @@
 package com.globomatics.bike.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String email;
     private String phone;
     private String model;
     private String serialNumber;
     private BigDecimal purchasePrice;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date purchaseDate;
     private boolean contact;
 }
